de2238ef56a83eb8e5493c35ca5ad5dea2c710e8
vivekshukla7374 <vivekshukla7374@gmail.com>
2025-07-21 23:18:19 +0530

    Download attribute remove from a tag.

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -23,13 +23,7 @@ import { LuArrowDownRight } from "react-icons/lu";
 const NavbarBtn = () => {
   return (
     <button className=" px-4 py-2 rounded-full text-xl font-bold font-body text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-darkCyan to-orange transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow">
-      <a
-        href="/Vivek_Shukla_CV.pdf" // 👉 place your PDF in the public folder
-        download
-        className="w-full h-full"
-      >
-        Hire Me
-      </a>
+     <a href="/Vivek_Shukla_CV.pdf" className="w-full h-full" > Hire Me </a>
       <div className="sm:hidden md:block">
         <LuArrowDownRight />
       </div>
