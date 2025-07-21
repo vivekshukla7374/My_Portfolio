22c3edb2c5d7be73c10ec993f439b5a82eaf3ed0
vivekshukla7374 <vivekshukla7374@gmail.com>
2025-07-21 12:23:18 +0530

    hero text fixed

--- src/components/heroSection/HeroText.jsx
+++ src/components/heroSection/HeroText.jsx
@@ -57,12 +57,12 @@ const HeroText = () => {
         <h1>Vivek Shukla</h1>
         <h3>And I'm a</h3>
       </motion.h2>
-      <motion.h1
+      <motion.h2
         variants={fadeIn("right", 0.4)}
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0 }}
-        className="md:text-[2.2rem] lg:text-5xl sm:text-3xl text-orange font-bold uppercase h-[120px]"
+        className="md:text-[2.2rem] lg:text-5xl sm:text-3xl text-orange font-bold uppercase h-[60px] m-0"
       >
         <Typewriter
           words={["Frontend Developer", "Web Developer", "UI/UX Designer","Coder"]}
@@ -73,14 +73,14 @@ const HeroText = () => {
           deleteSpeed={50}
           delaySpeed={1500}
         />
-      </motion.h1>
+      </motion.h2>
 
       <motion.p
         variants={fadeIn("up", 0.6)}
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0 }}
-        className="text-lg mt-4"
+        className="text-lg m-0"
       >
         "I'm Vivek, Passionate Web Developer & Student Instructor with 1 year of experience in <br />creating responsive websites and mentoring peers in web development basics."
       </motion.p>
--- src/components/navbar/NavbarMain.jsx
+++ src/components/navbar/NavbarMain.jsx
@@ -8,7 +8,7 @@ const NavbarMain = () => {
   const menuOpen = useSelector((state) => state.menu.menuOpen);
   return (
     <nav className="max-w-[1300px] mx-auto w-full  px-4 fixed left-[50%] -translate-x-[50%] z-20 flex gap-4 mt-2">
-      <div className="flex justify-between w-full max-w-[1200px] mx-auto  bg-black items-center p-6 rounded-r-full rounded-l-full border-orange border-[0.5px] ">
+      <div className="flex justify-between w-full max-w-[1200px] mx-auto  bg-black items-center p-3 px-6 rounded-[2rem] border-orange border-[0.5px] ">
         <NavbarLogo />
         <div className={`${menuOpen ? "sm:block" : "sm:hidden"} lg:block`}>
           <NavbarLinks />
@@ -16,7 +16,7 @@ const NavbarMain = () => {
 
         <NavbarBtn />
       </div>
-      <div className="flex lg:hidden sm:block p-6 bg-black items-center justify-center rounded-full  border-orange border-[0.5px] ">
+      <div className="flex lg:hidden sm:block p-3 bg-black items-center justify-center rounded-full  border-orange border-[0.5px] ">
         <NavbarToggler />
       </div>
     </nav>
